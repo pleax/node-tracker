700dbe6ee2bdc13d524fcf68f4d4a01687d47dca
pleax <aka.pleax@gmail.com>
2010-10-23 18:55:45 +0400

    Announce formatter now supports compact mode

--- test/formatters.js
+++ test/formatters.js
@@ -69,3 +69,97 @@ var formatters = require('../lib/formatters.js');
   }));
 })();
 
+
+(function() {
+  var torrentInfo = {
+    complete: 0,
+    incomplete: 0
+  };
+  var wantedPeers = [];
+  assert.equal(formatters.announce(torrentInfo, wantedPeers, true), b.encode({
+    interval: 10,
+    complete: 0,
+    incomplete: 0,
+    peers: ""
+  }));
+})();
+
+(function() {
+  var torrentInfo = {
+    complete: 10,
+    incomplete: 30
+  };
+  var wantedPeers = [];
+  assert.equal(formatters.announce(torrentInfo, wantedPeers, true), b.encode({
+    interval: 10,
+    complete: 10,
+    incomplete: 30,
+    peers: ""
+  }));
+})();
+
+(function() {
+  var torrentInfo = {
+    complete: 10,
+    incomplete: 30
+  };
+  var wantedPeers = [
+    { id: "peerId-8901234567890", ip: "192.0.32.10", port: 6337 }
+  ];
+  assert.equal(formatters.announce(torrentInfo, wantedPeers, true), b.encode({
+    interval: 10,
+    complete: 10,
+    incomplete: 30,
+    peers: "\xc0\x00\x20\x0a\x18\xc1"
+  }));
+})();
+
+(function() {
+  var torrentInfo = {
+    complete: 10,
+    incomplete: 30
+  };
+  var wantedPeers = [
+    { id: "peerId-8901234567890", ip: "192.0.32.10", port: 6337 },
+    { id: "peerId-8901234567890", ip: "192.0.32.10", port: 6339 }
+  ];
+  assert.equal(formatters.announce(torrentInfo, wantedPeers, true), b.encode({
+    interval: 10,
+    complete: 10,
+    incomplete: 30,
+    peers: "\xc0\x00\x20\x0a\x18\xc1\xc0\x00\x20\x0a\x18\xc3"
+  }));
+})();
+
+(function() {
+  var torrentInfo = {
+    complete: 10,
+    incomplete: 30
+  };
+  var wantedPeers = [
+    { id: "peerId-8901234567890", ip: "example.com", port: 6337 }
+  ];
+  assert.equal(formatters.announce(torrentInfo, wantedPeers, true), b.encode({
+    interval: 10,
+    complete: 10,
+    incomplete: 30,
+    peers: ""
+  }));
+})();
+
+(function() {
+  var torrentInfo = {
+    complete: 10,
+    incomplete: 30
+  };
+  var wantedPeers = [
+    { id: "peerId-8901234567890", ip: "a.b.example.com", port: 6337 }
+  ];
+  assert.equal(formatters.announce(torrentInfo, wantedPeers, true), b.encode({
+    interval: 10,
+    complete: 10,
+    incomplete: 30,
+    peers: ""
+  }));
+})();
+
