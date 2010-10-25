d1fa55683ec5db373657799135f158613d205d39
pleax <aka.pleax@gmail.com>
2010-10-26 02:30:56 +0400

    Renamed test files

--- test/suite.js
+++ test/suite.js
@@ -1,6 +1,6 @@
-require('./bencode.js');
-require('./utils.js');
-require('./dispatcher.js');
-require('./formatters.js');
-require('./handlers.js');
-require('./pool.js');
+require('./test-bencode.js');
+require('./test-utils.js');
+require('./test-dispatcher.js');
+require('./test-formatters.js');
+require('./test-handlers.js');
+require('./test-pool.js');
